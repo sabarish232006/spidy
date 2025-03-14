7f8ca43be79776679d1b59edced7355be5327c83
sabarish232006 <s9910843@gmail.com>
2025-03-14 11:33:41 +0530

    java

---
+++ decree.java
@@ -0,0 +1,12 @@
+public class decree
+{
+    public static void main(String args[])
+    {
+        int i=1;
+        for(i=100;i>=10;i--)
+        {
+            System.out.println(i);
+        }
+    }
+    
+}
