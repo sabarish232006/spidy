c442920bcc7889872c9d8bab2a2f9d093be47746
sabarish232006 <s9910843@gmail.com>
2025-03-14 10:14:20 +0530

    java

---
+++ comparision.java
@@ -0,0 +1,16 @@
+public class comparision
+{
+    public static void main(String args[])
+    {
+        Integer int1=56;
+        Integer int2=56;
+        if(int1 == int2)
+        {
+            System.out.println("true");
+        }
+        else
+        {
+            System.out.println("false");
+        }
+    }
+}
