680182f0484e3d1f41c3f65ab0429b85bbcc768a
sabarish232006 <s9910843@gmail.com>
2025-03-14 11:14:26 +0530

    java

---
+++ incree.java
@@ -0,0 +1,12 @@
+public class incree
+{
+    public static void main(String args[])
+    {
+        int i=1;
+        for(i=0;i<=10;i++)
+        {
+            System.out.println(i);
+        }
+    }
+    
+}
