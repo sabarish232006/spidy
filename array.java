e42b16946b57be5d532429540d4ec8d27912e59d
sabarish232006 <s9910843@gmail.com>
2025-03-14 14:03:29 +0530

    java

---
+++ array.java
@@ -0,0 +1,13 @@
+public class array
+{
+    public static void main(String args[])
+    {
+        String[] str={"rahulu","manilu","nitheishalu","princelu"};
+        System.out.println("at index [0]:"+str[0]);
+        System.out.println("at index [1]:"+str[1]);
+        System.out.println("at index [2]:"+str[2]);
+        System.out.println("at index [3]:"+str[3]);
+        System.out.println("length of string:"+str.length);
+
+    }
+}
