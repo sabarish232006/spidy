2324ce7219879af7a0ac7b4e1518b21397b1c77f
sabarish232006 <s9910843@gmail.com>
2025-03-14 13:25:41 +0530

    java

---
+++ multi.java
@@ -0,0 +1,14 @@
+public class multi
+{
+    static void multi()
+    {
+        int a=3;
+        int b=4;
+        int c=a*b;
+        System.out.println(c);
+    }
+    public static void main (String args[])
+    {
+        multi();
+    }
+}
