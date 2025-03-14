4bb9a0396b8540a863a8fe570ac02bfdfc351a00
sabarish232006 <s9910843@gmail.com>
2025-03-14 11:02:26 +0530

    java

---
+++ dow.java
@@ -0,0 +1,13 @@
+public class dow
+{
+    public static void main(String args[])
+    {
+        int i=6;
+        do
+        {
+            System.out.println(i);
+            i++;
+        }
+        while(i<=5);
+    }
+}
