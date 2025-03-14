a18df5c59dbc99f6b6640be01cd22758a44ebdab
sabarish232006 <s9910843@gmail.com>
2025-03-14 09:41:14 +0530

    java

---
+++ cmp.java
@@ -0,0 +1,16 @@
+public class cmp
+{
+    public static void main(String args[])
+    {
+        String str1="sabarish";
+        String str2="sab";
+        if(str1 == str2)
+        {
+            System.out.println("true");
+        }
+        else
+        {
+            System.out.println("false");
+        }
+    }
+}
