9e0fe908a0d2d21c6b80b3fa6eb20a58b75dad62
sabarish232006 <s9910843@gmail.com>
2025-03-14 10:34:37 +0530

    java

---
+++ vowel.java
@@ -0,0 +1,15 @@
+public class vowel
+{
+    public static void main(String args[])
+    {
+        String str="i like apple";
+        if(str.contains("apple"))
+        {
+            System.out.println("string contains apple");
+        }
+        else
+        {
+            System.out.println("string does not contains apple");
+        }
+    }
+}
