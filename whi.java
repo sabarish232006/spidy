d83ea9773687e217b689d36a2071f58e65602b0a
sabarish232006 <s9910843@gmail.com>
2025-03-14 10:50:32 +0530

    java

---
+++ whi.java
@@ -0,0 +1,12 @@
+public class whi
+{
+    public static void main(String args[])
+    {
+        int i=1;
+        while(i<=5)
+        {
+            System.out.println(10);
+            i++;
+        }
+    }
+}
